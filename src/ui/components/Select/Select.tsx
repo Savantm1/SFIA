6e1a36b56ace4d939eef08eb7afe8eb1855f8bfe
result <result007@yandex.ru>
2023-01-29 10:50:52 +0300

    add useMemo for optionsList, fix deafultSelectValue, fix type error with handleChange

--- src/ui/components/Select/Select.tsx
+++ src/ui/components/Select/Select.tsx
@@ -1,26 +1,31 @@
-import { FC, memo, useState } from 'react';
+import { FC, memo, useMemo, useState } from 'react';
 import { SelectChangeEvent } from '@mui/material';
 import { Styled } from '@ui/components/Select/styled';
 import { SelectProps } from '@ui/components/Select/types';
 
 export const Select: FC<SelectProps> = memo(
-    ({ options, defaultValue, placeholder, onSelect }) => {
+    ({ options, defaultValue = options[0].value, placeholder, onSelect }) => {
         const [selectedValue, setSelectedValue] = useState(
-            defaultValue ? defaultValue : placeholder ? '' : options[0].value
+            placeholder ? '' : defaultValue
         );
 
-        const handleChange = ({ target: { value } }: SelectChangeEvent) => {
+        const handleChange = (event: SelectChangeEvent<unknown>): void => {
+            const value = event.target.value as string;
             setSelectedValue(value);
             onSelect(value);
         };
 
-        const optionsList = options.map(({ value, text }) => {
-            return (
-                <Styled.Option key={value} value={value}>
-                    {text}
-                </Styled.Option>
-            );
-        });
+        const optionsList = useMemo(
+            () =>
+                options.map(({ value, text }) => {
+                    return (
+                        <Styled.Option key={value} value={value}>
+                            {text}
+                        </Styled.Option>
+                    );
+                }),
+            [options]
+        );
 
         return (
             <Styled.Select
