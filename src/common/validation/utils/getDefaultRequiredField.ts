19907443a50b41b4ac5a94bad02390ec37a195ca
result <result007@yandex.ru>
2023-02-14 00:48:05 +0300

    add types for getDefaultRequiredField utils

--- src/common/validation/utils/getDefaultRequiredField.ts
+++ src/common/validation/utils/getDefaultRequiredField.ts
@@ -1,9 +1,23 @@
-export const getDefaultRequiredField = (
-    field: string,
-    placeholder: string,
-    validations?: object
+export type FieldType = {
+    name: string;
+    placeholder: string;
+    initialValue?: string;
+    validations?: object;
+};
+
+type getDefaultRequiredFieldType = (
+    field: FieldType['name'],
+    placeholder: FieldType['placeholder'],
+    validations?: FieldType['validations']
+) => FieldType;
+
+export const getDefaultRequiredField: getDefaultRequiredFieldType = (
+    field,
+    placeholder,
+    validations
 ) => {
     return {
+        name: field,
         placeholder: placeholder,
         validations: {
             required: {
